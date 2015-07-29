e2f94887de850513698bf0fb6770d822c98af4d5
stuartsy <stuartsy96@gmail.com>
2015-07-29 14:54:55 +0800

    future questions no longer displayed

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -11,8 +12,13 @@ class IndexView(generic.ListView):
 	context_object_name = 'latest_question_list'
 
 	def get_queryset(self):
-		"""Return the last five published questions."""
-		return Question.objects.order_by('-pub_date')[:5]
+		"""
+		Return the last five published questions.
+		(not including those set to be published in the future)
+		"""
+		return Question.objects.filter(
+			pub_date__lte=timezone.now() #pub_date is <= timezone.now()
+		).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
 	model = Question
@@ -21,19 +27,6 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
 	model = Question
 	template_name = 'polls/results.html'
-
-# def index(request):
-# 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-# 	context = {'latest_question_list': latest_question_list}
-# 	return render(request, 'polls/index.html', context)
-
-# def detail(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/detail.html', {'question': question})
-
-# def results(request, question_id):
-# 	question = get_object_or_404(Question, pk=question_id)
-# 	return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
 	p = get_object_or_404(Question, pk=question_id)
